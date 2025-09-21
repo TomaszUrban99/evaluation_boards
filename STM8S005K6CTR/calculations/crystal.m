15fad4c48afcc9f2a974173fee1c874459019402
Tomasz Urban <tomasz.urban.116@gmail.com>
2025-09-21 12:55:26 +0200

    [STM8S005K6T6CTR] Board scheme: Improvements

--- STM8S005K6CTR/calculations/crystal.m
+++ STM8S005K6CTR/calculations/crystal.m
@@ -1,11 +1,11 @@
 % STM8S005 - crystal calculation
 fhse = 8 * 10^6; % HSE frequency [Hz]
-C0 = 7 * 10^(-12); % Shunt capacitance [F]
-CL = 16 * 10^(-12); % Load capacitance [F]
+C0 = 2 * 10^(-12); % Shunt capacitance [F]
+CL = 8 * 10^(-12); % Load capacitance [F]
 Cs = 5 * 10^(-12); % Stray capacitance [F]
 
 ESR = 40; % [Ohm]
-DL = 500 * 10^(-6); % Drive level [W]
+DL = 200 * 10^(-6); % Drive level [W]
 
 % CL1 = CL2 - capacitance of one capacitor
 CL1 = 2 * (CL - Cs); 
---
+++ components/clean_lib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -r A*
+rm -r C*
+rm -r D*
+rm -r E*
+rm -r e*
+rm -r O*
+rm -r P*
+rm -r Q*
+rm -r S*
+rm -r T*
+rm -r x*
+rm -r X*
